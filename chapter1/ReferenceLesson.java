16997f2bcbb9a9aec912da96512431129be086bd
Jaypee Vilador <70680277+jypeejypee@users.noreply.github.com>
2026-09-25 02:24:02 +0000

    day 12 - ReferenceLesson

---
+++ chapter1/ReferenceLesson.java
@@ -0,0 +1,13 @@
+package chapter1;
+
+public class ReferenceLesson {
+
+public static void main(String[] args) {
+    java.util.Date today = new java.util.Date();
+    String greeting = "Hi Jaypee";
+
+    System.out.println(today.getTime());
+    System.out.println(greeting);
+    System.out.println(args[1]);
+}
+}
